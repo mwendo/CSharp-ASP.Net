3a2ee92aa5aaf04095391313aa9718672337bbf7
Micah Wendorf <wendorfmicah0@gmail.com>
2021-06-10 13:52:16 -0500

    first commit

---
+++ TimeDisplay/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimeDisplay.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult DisplayTime()
+        {
+            return View();
+        }
+    }
+}
